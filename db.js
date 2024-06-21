d89592190d10af14822489ee7fc3c65903ff09cd
kumarrau <raushan.kumar2@otis.com>
2024-06-22 02:47:11 +0530

    added frontend

--- db.js
+++ db.js
@@ -4,6 +4,7 @@
  * description : string
  * completed:boolean
  * }
+ * https://cloud.mongodb.com/v2/65a00d4a3a198533b829fbc0#/metrics/replicaSet/6675d23a2f279843017bec77/explorer/todos/todos/find
  * 
  */
 const mongoose = require("mongoose");
